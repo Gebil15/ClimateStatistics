d58ef6997a6ca05d7e1a1bbed333df75ebd178db
Gebil15 <joheubi15@gmail.com>
2016-11-10 14:37:47 +0100

    bck

--- src/climate/client/ClimateData.java
+++ src/climate/client/ClimateData.java
@@ -6,7 +6,6 @@ public class ClimateData implements Serializable {
 
 	private String message;
 	public ClimateData(){};
-	private int a;
 	
 	public void setMessage(String message) {
 		this.message = message;
